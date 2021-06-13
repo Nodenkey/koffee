10b65bac3adb6779f541793fa3db9e64325ba284
nii <nodenkey@gmail.com>
2021-06-13 11:43:11 +0000

    Change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `koffee`,
+    description: `Koffee offers a wide range of coffee products to satisfy all your coffee needs.`,
     author: `@gatsbyjs`,
   },
   plugins: [
